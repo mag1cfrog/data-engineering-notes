72567d35e391934dc5c1734ca056a29557513b57
hanbow <harrywong2017@gmail.com>
2025-06-17 15:42:48 -0700

    feat: add custom table of contents integration and update blog post metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import customToc from "astro-custom-toc";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mag1cfrog.github.io',
 	base: '/spark-tuning-notes/',
 	output: 'static',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		customToc(
+			{
+            maxDepth: 4, // Include headings up to level 4 (####)
+        }
+		), 
+		mdx(), 
+		sitemap()],
 });
